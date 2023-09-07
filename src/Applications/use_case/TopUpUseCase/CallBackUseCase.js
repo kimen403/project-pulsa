f3949a3216360d5cf287ad3767908f3e712de688
Kimens <apaadanya1700@gmail.com>
2023-09-07 17:06:31 +0200

    tes

--- src/Applications/use_case/TopUpUseCase/CallBackUseCase.js
+++ src/Applications/use_case/TopUpUseCase/CallBackUseCase.js
@@ -39,7 +39,8 @@ class CallBackUseCase {
           console.log('masuk capture');
           console.log('userId', userId);
           await this._topUpRepository.updateStatus(useCasePayload.order_id, 'success');
-          await this._userRepository.updateBalance(userId, Number.parseInt(useCasePayload.gross_amount));
+          // eslint-disable-next-line radix
+          await this._userRepository.updateBalance(userId, parseInt(useCasePayload.gross_amount.replace('.', '')));
           break;
         case 'pending':
           // Perform actions for `pending` case
