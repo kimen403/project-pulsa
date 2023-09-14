9e071aeb6775419f2758eac7c911e3996d6584a3
Kimens <apaadanya1700@gmail.com>
2023-09-14 11:39:32 +0200

    first

--- src/Interfaces/http/api/service/routes.js
+++ src/Interfaces/http/api/service/routes.js
@@ -34,15 +34,6 @@ const routes = (handler) => [
       },
     },
   },
-
-  // {
-  //     method: 'DELETE',
-  //     path: '/threads/{threadId}/comments/{commentId}',
-  //     handler: handler.deleteCommentHandler,
-  //     options: {
-  //         auth: 'forumapi_jwt',
-  //     },
-  // },
 ];
 
 module.exports = routes;
